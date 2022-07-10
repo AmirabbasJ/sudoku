b0229ba3ecc4c69049df17e8b2ad2a75ec252107
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-11 00:48:35 +0430

    feat: add selecting mechanizm and styling

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/no-array-index-key */
+import { useState } from 'react';
 import styled from 'styled-components';
 
+import type { Id } from '../domain/Board';
+import { toId, updateBoard } from '../domain/Board';
 import { getBoard } from '../getBoard';
 
 const Container = styled.div`
@@ -26,25 +29,43 @@ const Block = styled.div`
   grid-template-rows: 1fr 1fr 1fr;
 `;
 
-const Slot = styled.div`
+const Slot = styled.div<{ isSelected: boolean }>`
   width: 4rem;
-  background-color: #ebebeb;
+  background-color: ${({ isSelected }) => (isSelected ? '#d1d1d1' : '#ebebeb')};
   height: 4rem;
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 1.3em;
+  cursor: pointer;
 `;
-
-export const Board: React.FC = () => (
-  <Container>
-    {getBoard()
-      .flat()
-      .map((rowBlock, bi) => (
-        <Block key={bi}>
-          {rowBlock.map((block, bli) =>
-            block.map((slot, si) => <Slot key={bi + bli + si}>{slot}</Slot>),
-          )}
-        </Block>
-      ))}
-  </Container>
-);
+const initBoard = getBoard();
+export const Board: React.FC = () => {
+  const [board, setBoard] = useState(initBoard);
+  const [selectedId, setSelectedId] = useState<Id | null>(null);
+  return (
+    <Container>
+      {board.map((rowBlock, bi) =>
+        rowBlock.map((b, rbi) => (
+          <Block key={bi}>
+            {b.map((block, bli) =>
+              block.map((slot, si) => {
+                const id = toId(bi, rbi, bli, si);
+                return (
+                  <Slot
+                    isSelected={id === selectedId}
+                    key={id}
+                    // onClick={() => setBoard(updateBoard(board, id, 'empty'))}
+                    onClick={() => setSelectedId(id)}
+                  >
+                    {slot}
+                  </Slot>
+                );
+              }),
+            )}
+          </Block>
+        )),
+      )}
+    </Container>
+  );
+};
