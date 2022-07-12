e938d0e46d2b35c50cc0df41b843befa4bb26183
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-12 17:22:19 +0430

    fix: fix prettier format

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,23 +1,18 @@
 module.exports = {
-  arrowParens:
-    'avoid',
+  arrowParens: 'avoid',
   bracketSpacing: true,
   endOfLine: 'lf',
-  htmlWhitespaceSensitivity:
-    'css',
+  htmlWhitespaceSensitivity: 'css',
   insertPragma: false,
   bracketSameLine: false,
   jsxSingleQuote: false,
   printWidth: 120,
-  proseWrap:
-    'always',
-  quoteProps:
-    'consistent',
+  proseWrap: 'always',
+  quoteProps: 'consistent',
   requirePragma: false,
   semi: true,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma:
-    'all',
+  trailingComma: 'all',
   useTabs: false,
 };
