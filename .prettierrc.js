eec776cb7986cd702d17fafd240092e9ebc36ef2
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-12 14:43:21 +0430

    config: add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  arrowParens:
+    'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity:
+    'css',
+  insertPragma: false,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap:
+    'always',
+  quoteProps:
+    'consistent',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma:
+    'all',
+  useTabs: false,
+};
