c3c9cbb87669f2c9b8e5413a1e204ea0278a449c
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-12 17:25:26 +0430

    refactor: use boardIndex as name

--- src/domain/Board.ts
+++ src/domain/Board.ts
@@ -12,14 +12,13 @@ export type BoardIndex = [number, number, number, number];
 
 export const toId = (bi: number, rbi: number, bli: number, si: number): Id => `${bi}-${rbi}-${bli}-${si}`;
 
-export const fromIdToIndex = (id: Id): BoardIndex => id.split('-').map(x => Number.parseInt(x, 10)) as BoardIndex;
-
+export const fromIdToBoardIndex = (id: Id): BoardIndex => id.split('-').map(x => Number.parseInt(x, 10)) as BoardIndex;
 export const parseSlot = (x: string): Slot | null =>
   /^(Backspace)|(Delete)$/.test(x) ? '' : /^([1-9]{1})$/.test(x) ? (Number.parseInt(x, 10) as NumericSlot) : null;
 
 export const editSlot = (initBoard: Board, id: Id, key: string) => {
   const slot = parseSlot(key);
   if (slot == null) return initBoard;
-  const indexes = fromIdToIndex(id);
-  return R.assocPath(indexes, slot, initBoard);
+  const boardIndex = fromIdToBoardIndex(id);
+  return R.assocPath(boardIndex, slot, initBoard);
 };
