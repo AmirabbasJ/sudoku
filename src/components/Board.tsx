ebc97d36b75e7c018a951d48733175b02fc204bc
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-12 17:23:56 +0430

    feat: add edit mechanizm

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,9 @@
 /* eslint-disable react/no-array-index-key */
+import { useState } from 'react';
 import styled from 'styled-components';
 
+import type { Id } from '../domain/Board';
+import { editSlot, toId } from '../domain/Board';
 import { getBoard } from '../getBoard';
 
 const Container = styled.div`
@@ -26,25 +29,63 @@ const Block = styled.div`
   grid-template-rows: 1fr 1fr 1fr;
 `;
 
-const Slot = styled.div`
+const Slot = styled.div<{ isSelected: boolean }>`
   width: 4rem;
-  background-color: #ebebeb;
+  background-color: ${({ isSelected }) => (isSelected ? '#d1d1d1' : '#ebebeb')};
   height: 4rem;
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 1.3em;
+  cursor: pointer;
+  user-select: none;
+  outline: none;
 `;
 
-export const Board: React.FC = () => (
-  <Container>
-    {getBoard()
-      .flat()
-      .map((rowBlock, bi) => (
-        <Block key={bi}>
-          {rowBlock.map((block, bli) =>
-            block.map((slot, si) => <Slot key={bi + bli + si}>{slot}</Slot>),
-          )}
-        </Block>
-      ))}
-  </Container>
-);
+const initBoard = getBoard();
+export const Board: React.FC = () => {
+  const [board, setBoard] = useState(initBoard);
+  const [selectedId, setSelectedId] = useState<Id | null>(null);
+
+  const onSlotChange = (key: string) =>
+    setBoard(editSlot(board, selectedId as Id, key));
+  const toggleFocus = (target: HTMLDivElement, isSelected: boolean, id: Id) => {
+    if (isSelected) {
+      setSelectedId(null);
+      target.blur();
+      return;
+    }
+    setSelectedId(id);
+    target.focus();
+  };
+
+  return (
+    <Container>
+      {board.map((rowBlock, bi) =>
+        rowBlock.map((b, rbi) => (
+          <Block key={rbi + bi}>
+            {b.map((block, bli) =>
+              block.map((slot, si) => {
+                const id = toId(bi, rbi, bli, si);
+                const isSelected = id === selectedId;
+                return (
+                  <Slot
+                    isSelected={isSelected}
+                    key={id}
+                    tabIndex={0}
+                    onKeyDown={({ key }) => onSlotChange(key)}
+                    onClick={({ currentTarget }) =>
+                      toggleFocus(currentTarget, isSelected, id)
+                    }
+                  >
+                    {slot}
+                  </Slot>
+                );
+              }),
+            )}
+          </Block>
+        )),
+      )}
+    </Container>
+  );
+};
--- src/domain/Board.ts
+++ src/domain/Board.ts
@@ -1,17 +1,25 @@
-// export type Slot = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-// export type RowSlot = [Slot, Slot, Slot];
-// export type Block = [RowSlot, RowSlot, RowSlot];
-// export type RowBlock = [Block, Block, Block];
-// export type Board = [RowBlock, RowBlock, RowBlock];
+import * as R from 'ramda';
 
-export type Slot = 'empty' | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-export type Block = [
-  [Slot, Slot, Slot],
-  [Slot, Slot, Slot],
-  [Slot, Slot, Slot],
-];
-export type Board = [
-  [Block, Block, Block],
-  [Block, Block, Block],
-  [Block, Block, Block],
-];
+type NumericSlot = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+type EmptySlot = '';
+export type Slot = EmptySlot | NumericSlot;
+
+export type Block = [[Slot, Slot, Slot], [Slot, Slot, Slot], [Slot, Slot, Slot]];
+export type Board = [[Block, Block, Block], [Block, Block, Block], [Block, Block, Block]];
+
+export type Id = `${number}-${number}-${number}-${number}`;
+export type BoardIndex = [number, number, number, number];
+
+export const toId = (bi: number, rbi: number, bli: number, si: number): Id => `${bi}-${rbi}-${bli}-${si}`;
+
+export const fromIdToIndex = (id: Id): BoardIndex => id.split('-').map(x => Number.parseInt(x, 10)) as BoardIndex;
+
+export const parseSlot = (x: string): Slot | null =>
+  /^(Backspace)|(Delete)$/.test(x) ? '' : /^([1-9]{1})$/.test(x) ? (Number.parseInt(x, 10) as NumericSlot) : null;
+
+export const editSlot = (initBoard: Board, id: Id, key: string) => {
+  const slot = parseSlot(key);
+  if (slot == null) return initBoard;
+  const indexes = fromIdToIndex(id);
+  return R.assocPath(indexes, slot, initBoard);
+};
