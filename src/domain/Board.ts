70fa47925dfd3f128f0bbe9a81ee14191b79b53c
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-13 16:36:37 +0430

    feat: add checkIsInBlock

--- src/domain/Board.ts
+++ src/domain/Board.ts
@@ -9,20 +9,35 @@ export type Board = [[Block, Block, Block], [Block, Block, Block], [Block, Block
 
 export type Id = `${number}-${number}-${number}-${number}`;
 export type BoardIndex = [number, number, number, number];
-
 export const toId = (bi: number, rbi: number, bli: number, si: number): Id => `${bi}-${rbi}-${bli}-${si}`;
 
-export const fromIdToBoardIndex = (id: Id): BoardIndex => id.split('-').map(x => Number.parseInt(x, 10)) as BoardIndex;
+export const idToBoardIndex = (id: Id): BoardIndex => id.split('-').map(x => Number.parseInt(x, 10)) as BoardIndex;
 export const parseSlot = (x: string): Slot | null =>
   /^(Backspace)|(Delete)$/.test(x) ? '' : /^([1-9]{1})$/.test(x) ? (Number.parseInt(x, 10) as NumericSlot) : null;
 
-const setSlot = (initBoard: Board, id: Id, slot: Slot) => {
-  const boardIndex = fromIdToBoardIndex(id);
-  return R.assocPath(boardIndex, slot, initBoard);
-}
+const setSlot = (board: Board, id: Id, slot: Slot) => {
+  const boardIndex = idToBoardIndex(id);
+  return R.assocPath(boardIndex, slot, board);
+};
 
-export const editSlot = (initBoard: Board, id: Id, key: string) => {
+const checkIsInBlock = (board: Board, id: Id, slot: NumericSlot): boolean => {
+  const [blockRow, blockCol] = idToBoardIndex(id);
+  const block = board[blockRow]![blockCol]!;
+  const blockNumbers = block.flat();
+  return blockNumbers.includes(slot);
+};
+// const getRow = () => {};
+// const getCol = () => {};
+
+const checkIsValidSlot = (board: Board, id: Id, key: NumericSlot): boolean => !checkIsInBlock(board, id, key);
+
+export const editSlot = (board: Board, id: Id, key: string): Board | 'failure' => {
   const slot = parseSlot(key);
-  if (slot == null) return initBoard;
-  return setSlot(initBoard, id, slot)
+  if (slot == null) return board;
+  if (slot === '') return setSlot(board, id, slot);
+
+  const isValidSlot = checkIsValidSlot(board, id, slot);
+  if (!isValidSlot) return 'failure';
+
+  return setSlot(board, id, slot);
 };
