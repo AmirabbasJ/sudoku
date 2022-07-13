57553dc44aee889ff02d2476b9c90809ed9a3104
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-13 21:23:38 +0430

    refactor: refactor slotValidation

--- src/domain/Board.ts
+++ src/domain/Board.ts
@@ -13,8 +13,8 @@ export type BoardIndex = [number, number, number, number];
 
 export const toId = (blockRowIndex: number, blockColIndex: number, slotRowIndex: number, slotColIndex: number): Id =>
   `${blockRowIndex}-${blockColIndex}-${slotRowIndex}-${slotColIndex}`;
-
 export const idToBoardIndex = (id: Id): BoardIndex => id.split('-').map(x => Number.parseInt(x, 10)) as BoardIndex;
+
 export const parseSlot = (x: string): Slot | null =>
   /^(Backspace)|(Delete)$/.test(x) ? '' : /^([1-9]{1})$/.test(x) ? (Number.parseInt(x, 10) as NumericSlot) : null;
 
@@ -23,28 +23,24 @@ const setSlot = (board: Board, id: Id, slot: Slot) => {
   return R.assocPath(boardIndex, slot, board);
 };
 
-const checkIsInBlock = (board: Board, id: Id, slot: NumericSlot): boolean => {
+const isNotInBlock = (board: Board, id: Id, slot: NumericSlot): boolean => {
   const [blockRow, blockCol] = idToBoardIndex(id);
   const block = board[blockRow]![blockCol]!;
-  const blockNumbers = block.flat();
-  return blockNumbers.includes(slot);
+  const blockSlots = block.flat();
+  return !blockSlots.includes(slot);
 };
 
-const checkIsInRow = (board: Board, id: Id, slot: NumericSlot): boolean => {
+const isNotInRow = (board: Board, id: Id, slot: NumericSlot): boolean => {
   const [blockRow, _, slotRow] = idToBoardIndex(id);
-  const row = R.pluck(slotRow, board[blockRow]!).flat();
-  return row.includes(slot);
+  const rowSlots = R.pluck(slotRow, board[blockRow]!).flat();
+  return !rowSlots.includes(slot);
 };
 // const getCol = () => {};
 
-const checkIsValidSlot = (board: Board, id: Id, key: Slot): boolean => {
-  if (key === '') return true;
-  console.log(checkIsInRow(board, id, key));
-  return !checkIsInRow(board, id, key) && !checkIsInBlock(board, id, key);
-};
+const isValidNumericSlot = R.allPass([isNotInRow, isNotInBlock]);
 
 export const editSlot = (board: Board, id: Id, slot: Slot): [Board, SlotState] => {
-  const isValidSlot = checkIsValidSlot(board, id, slot);
+  const isValidSlot = slot === '' || isValidNumericSlot(board, id, slot);
   const newBoard = setSlot(board, id, slot);
   const state: SlotState = !isValidSlot ? 'mistake' : 'correct';
   return [newBoard, state];
