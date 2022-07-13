f14e455d0b6e47f06d96111905752c04c5d6edd2
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-13 13:17:54 +0430

    feat: add robot font

--- src/GlobalStyles.tsx
+++ src/GlobalStyles.tsx
@@ -1,7 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
+import Roboto from './assets/Roboto-Light.ttf';
 
 export const GlobalStyle = createGlobalStyle`
-  *{
+  @font-face {
+    font-family: 'Roboto';
+    src: url(${Roboto}) format('truetype');
+  }
+  
+  *{    
+    font-family: 'Roboto';
     padding:0;
     margin:0;
     box-sizing:border-box;
