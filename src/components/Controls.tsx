7265d842cf0568be2e1f777121afe144d27c3f9f
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-18 10:27:24 +0430

    fix: fix erase btn style

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -7,7 +7,6 @@ import { EraseBtn } from './EraseBtn';
 const Div = styled.div`
   width: 100%;
   height: 100%;
-  background-color: red;
 `;
 
 export const Controls: React.FC = () => {
--- src/components/EraseBtn.tsx
+++ src/components/EraseBtn.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Button = styled.button``;
+const Button = styled.button`
+  padding: 1rem 2.25rem;
+  font-size: 1.3em;
+  background-color: transparent;
+  border: 0.12rem solid royalblue;
+  border-radius: 0.5rem;
+  color: royalblue;
+  cursor: pointer;
+  :hover {
+    border: 0.12rem solid white;
+    background-color: royalblue;
+    color: white;
+  }
+`;
 
 interface EraseBtnProps {
   onClick: () => void;
