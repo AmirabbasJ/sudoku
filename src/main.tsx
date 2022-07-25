f551b7d18db0307e41bd39d52b544386084a1ee9
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-25 23:02:46 +0430

    feat: disable everything when game is paused

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,19 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { BoardCtxProvider } from './context/BoardCtx';
 import { DraftCtxProvider } from './context/DraftCtx';
+import { GameStateCtxProvider } from './context/GameStateCtx';
 import { ThemeCtxProvider } from './context/ThemeCtx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeCtxProvider>
-      <DraftCtxProvider>
-        <BoardCtxProvider>
-          <App />
-        </BoardCtxProvider>
-      </DraftCtxProvider>
+      <GameStateCtxProvider>
+        <DraftCtxProvider>
+          <BoardCtxProvider>
+            <App />
+          </BoardCtxProvider>
+        </DraftCtxProvider>
+      </GameStateCtxProvider>
     </ThemeCtxProvider>
   </React.StrictMode>,
 );
