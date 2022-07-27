764564038958c10c7ca3d65ced2d919842cc7752
AmirabbasJ <amirabbasgamer2@gmail.com>
2022-07-27 18:31:01 +0430

    fix: remove prev code

--- src/hooks/useDraft.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-
-import { DraftCtx } from '../context/DraftCtx';
-
-export const useDraft = () => {
-  const { isDraftMode, setIsDraftMode } = useContext(DraftCtx);
-  const toggleDraftMode = () => setIsDraftMode(!isDraftMode);
-  return { isDraftMode, toggleDraftMode };
-};
