d8baad8a1d7ca3b26881a623a8cd834e6fb6af92
AmirabbasJ <amirabbasgamer2@gmail.com>
2023-02-13 20:13:13 +0330

    remove react query use

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -9,30 +8,18 @@ import { GameStateCtxProvider } from './context/GameStateCtx';
 import { ThemeCtxProvider } from './context/ThemeCtx';
 import { TimerCtxProvider } from './context/TimerCtx';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      retry: false,
-    },
-  },
-});
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeCtxProvider>
-        <GameStateCtxProvider>
-          <DraftCtxProvider>
-            <TimerCtxProvider>
-              <BoardCtxProvider>
-                <App />
-              </BoardCtxProvider>
-            </TimerCtxProvider>
-          </DraftCtxProvider>
-        </GameStateCtxProvider>
-      </ThemeCtxProvider>
-    </QueryClientProvider>
+    <ThemeCtxProvider>
+      <GameStateCtxProvider>
+        <DraftCtxProvider>
+          <TimerCtxProvider>
+            <BoardCtxProvider>
+              <App />
+            </BoardCtxProvider>
+          </TimerCtxProvider>
+        </DraftCtxProvider>
+      </GameStateCtxProvider>
+    </ThemeCtxProvider>
   </React.StrictMode>,
 );
